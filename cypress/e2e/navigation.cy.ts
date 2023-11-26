aea89199e73f935e227e24423fb7fef57f6907a3
KeithPetr <keith.petryshyn@gmail.com>
2023-11-26 01:23:18 -0500

    updated link to issues page when collapsed sidebar

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -68,7 +68,10 @@ describe("Sidebar Navigation", () => {
 
       // check that links still exist and are functionable
       cy.get("nav").find("a").should("have.length", 6).eq(1).click();
-      cy.url().should("eq", "http://localhost:3000/dashboard/issues");
+      cy.url().should(
+        "eq",
+        "http://localhost:3000/dashboard/issues?page=1&status=&level=&project=",
+      );
 
       // check that text is not rendered
       cy.get("nav").contains("Issues").should("not.exist");
